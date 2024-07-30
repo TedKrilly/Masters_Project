7a553c068a1c333b65336cf63320d856b1525b53
TedKrilly <129395561+TedKrilly@users.noreply.github.com>
2024-07-30 21:12:55 +0100

    Add files via upload

---
+++ EDES-Model/streamlined_fit_model.jl
@@ -0,0 +1,24 @@
+include("streamlined_func.jl")
+
+# generate results
+OGTTsimulates, OGTTestimates, OGTTresiduals, OGTTglu_bands, OGTTins_bands = gen_results(;test="OGTT");
+CGMsimulates, CGMestimates, CGMresiduals, CGMglu_bands, CGMins_bands = gen_results(;test="CGM");
+
+# save parameter estimates, simulation and residuals
+ogtts = stack(OGTTestimates, 3:11);
+CSV.write("OGTTestimates.csv", ogtts; transform=(col, val) -> something(val, missing))
+CSV.write("OGTTsimulates.csv", OGTTsimulates)
+CSV.write("OGTTresiduals.csv", OGTTresiduals)
+
+
+if isnothing(CGMsimulates)
+    println("No CGM data to process and save.")
+else
+    cgms = stack(CGMestimates, 3:14);
+    CSV.write("CGMestimates.csv", cgms; transform=(col, val) -> something(val, missing))
+    CSV.write("CGMsimulates.csv", CGMsimulates)
+    CSV.write("CGMresiduals.csv", CGMresiduals)
+end
+
+
+
